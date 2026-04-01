da31425d8bb99c9e33d093c86f7f68397fd964cf
ceyhunbaskoc <ceyhunbaskoc@gmail.com>
2026-04-01 23:05:03 +0300

    fix: map 401 error fixed

--- solveit-frontend/src/pages/MapPage.jsx
+++ solveit-frontend/src/pages/MapPage.jsx
@@ -184,8 +184,8 @@ const MapPage = () => {
         className="z-0"
       >
         <TileLayer
-          attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
         {/* User location marker */}
